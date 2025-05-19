3c47d5588470aafe5bbada68e83c57213c7c57c0
wlong <109259315+wlong1@users.noreply.github.com>
2025-05-18 21:09:00 -0400

    favicon change
    added site title and favicon

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -5,6 +5,7 @@ export default function Footer() {
     <footer className={styles.footer}>
       <div>Location: 160 Convent Ave, New York</div>
       <div>Contact: info@email.com | (555) 555-7000</div>
+      <div>Magnifying glass favicon created by <a href="https://www.flaticon.com/free-icons/search" title="search icons">Dewi Sari</a></div>
     </footer>
   );
 }
